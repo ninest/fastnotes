fb87bd7c4c604b25d6267629dfb9497a8ddb84fc
ninest <parth.kabra@gmail.com>
2020-09-18 00:56:40 +0800

    Add command/cntrl S saving

--- src/script.js
+++ src/script.js
@@ -3,12 +3,22 @@ const $note = document.querySelector("article");
 const save = () => {
   const text = $note.innerText;
   localStorage.note = text;
+  console.log("save");
 };
 const load = () => {
-  $note.innerText = localStorage.note || "";
+  if (localStorage.note !== undefined) $note.innerText = localStorage.note;
 };
 
 // Load previously saved note
 window.onload = load;
 // Save note on edit
-$note.onkeypress = save;
+$note.onkeyup = save;
+
+// control s
+window.addEventListener("keydown", (e) => {
+  if ((e.ctrlKey || e.metaKey) && e.key === "s") {
+    save();
+    console.log("saved");
+    e.preventDefault();
+  }
+});
