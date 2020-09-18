4f7aa5c957e5e3338c338cc2a563f42436c01f5b
ninest <parth.kabra@gmail.com>
2020-09-18 13:37:06 +0800

    Add links support and switch to textarea

--- src/script.js
+++ src/script.js
@@ -1,24 +1,45 @@
-const $note = document.querySelector("article");
+const $note = document.querySelector("textarea");
+const $linksList = document.querySelector("#links-list");
 
 const save = () => {
-  const text = $note.innerText;
+  const text = $note.value;
   localStorage.note = text;
-  console.log("save");
 };
 const load = () => {
-  if (localStorage.note !== undefined) $note.innerText = localStorage.note;
+  if (localStorage.note !== undefined) {
+    $note.value = localStorage.note;
+  }
+};
+
+const links = () => {
+  const text = $note.value;
+  const urlre =
+    /((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;
+  const urls = text.match(urlre) || [];
+
+  $linksList.innerHTML = "";
+  for (const url of urls) {
+    $linksList.innerHTML += `<li>
+      <a href="${url}"> ${url} </a>
+    </li>`;
+  }
 };
 
 // Load previously saved note
-window.onload = load;
+window.onload = () => {
+  load();
+  links();
+};
 // Save note on edit
-$note.onkeyup = save;
+$note.onkeyup = () => {
+  save();
+  links();
+};
 
 // control s
 window.addEventListener("keydown", (e) => {
   if ((e.ctrlKey || e.metaKey) && e.key === "s") {
     save();
-    console.log("saved");
     e.preventDefault();
   }
 });
