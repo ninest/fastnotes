d182bc0fba2d997d3a198a34841461a79b98d7c1
ninest <parth.kabra@gmail.com>
2021-01-26 23:36:44 +0800

    Add lz-string

--- src/script.js
+++ src/script.js
@@ -1,42 +1,33 @@
+import {
+  compressToEncodedURIComponent,
+  decompressFromEncodedURIComponent,
+} from "lz-string";
+
 const $note = document.querySelector("textarea");
-const $linksList = document.querySelector("#links-list");
 
 const save = () => {
   const text = $note.value;
-  localStorage.note = text;
+  // localStorage.note = text;
+  const compressed = compressToEncodedURIComponent(text);
+  history.replaceState(undefined, undefined, `#${compressed}`);
 };
 const load = () => {
-  if (localStorage.note !== undefined) {
-    $note.value = localStorage.note;
-  }
-};
-
-const links = () => {
-  const text = $note.value;
-  const urlre =
-    /((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;
-  const urls = text.match(urlre) || [];
-
-  $linksList.innerHTML = "";
-  for (const url of urls) {
-    $linksList.innerHTML += `<li>
-      <a href="${url}"> ${url} </a>
-    </li>`;
-  }
+  const text = decompressFromEncodedURIComponent(location.hash.substring(1));
+  console.log(text)
+  $note.value = text;
 };
 
 // Load previously saved note
 window.onload = () => {
   load();
-  links();
 };
 // Save note on edit
-$note.onkeyup = () => {
-  save();
-  links();
-};
+// $note.onkeyup = () => {
+//   save();
+//   links();
+// };
 
-// control s
+// Control S to save
 window.addEventListener("keydown", (e) => {
   if ((e.ctrlKey || e.metaKey) && e.key === "s") {
     save();
